46d5ada60b6d41134528f9c397a5be9f279d2348
Sriram <sriram.senthilkr@gmail.com>
2023-12-10 13:39:11 +0800

    Solved bottom white tab bug

--- components/LocationCard.js
+++ components/LocationCard.js
@@ -2,17 +2,48 @@ import { View, Text } from "react-native";
 
 export default function LocationCard({ location }) {
 	return (
-		<View style={{ borderWidth: 2, marginBottom: 10 }}>
-			<Text> Mcdonald </Text>
+		<View style={{ padding: 10, paddingTop: 7 }}>
+			<Text
+				style={{
+					fontSize: 27,
+					color: "#F9C49D",
+					marginBottom: 5,
+					fontWeight: "500",
+				}}
+			>
+				{" "}
+				McDonald's{" "}
+			</Text>
+			<Text
+				style={{
+					fontSize: 17,
+					fontWeight: "500",
+					color: "white",
+					marginBottom: 4,
+				}}
+			>
+				{" "}
+				Fast Food{" "}
+			</Text>
 			<View
 				style={{
 					flexDirection: "row",
 					alignItems: "center",
 				}}
 			>
-				<Text> Central </Text>
-				<Text> $$$ </Text>
-				<Text> V </Text>
+				<Text style={{ color: "white" }}> Central </Text>
+				<Text style={{ color: "white" }}> $$$ </Text>
+				<Text style={{ color: "white" }}> V </Text>
+			</View>
+			<View style={{ alignItems: "center", marginTop: 15 }}>
+				<View
+					style={{
+						borderWidth: 1.5,
+						borderColor: "#F9C49D",
+						borderRadius: 20,
+						width: 70,
+					}}
+				/>
 			</View>
 		</View>
 	);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,70 +10,94 @@ export default function HomeScreen() {
 		vegan: true,
 	};
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<View
-				style={{
-					flexDirection: "column",
-					height: "100%",
-					width: "100%",
-				}}
-			>
+		<SafeAreaView
+			edges={["top", "right", "left"]}
+			style={{
+				flex: 1,
+				backgroundColor: "white",
+				flexDirection: "column",
+			}}
+		>
+			<View style={{ flex: 1 }}>
 				<View style={{ flex: 1 }}>
-					<View style={{ flex: 1 }}>
-						<Text
+					<Text
+						style={{
+							fontWeight: "bold",
+							marginLeft: "12%",
+							marginTop: "5%",
+							fontSize: 30,
+						}}
+					>
+						What are you
+					</Text>
+					<Text
+						style={{
+							fontWeight: "bold",
+							marginLeft: "12%",
+							fontSize: 30,
+						}}
+					>
+						feeling today?
+					</Text>
+				</View>
+				<View style={{ flex: 2, justifyContent: "center" }}>
+					<View style={{}}>
+						<View
 							style={{
-								fontWeight: "bold",
-								marginLeft: "12%",
-								marginTop: "5%",
-								fontSize: 30,
+								flexDirection: "row",
+								justifyContent: "space-around",
+								marginBottom: "5%",
 							}}
 						>
-							What are you
-						</Text>
-						<Text
+							<Text
+								style={{
+									borderColor: "black",
+									borderRadius: 1,
+									borderWidth: 1,
+								}}
+							>
+								Area
+							</Text>
+							<Text>Cuisine</Text>
+						</View>
+						<View
 							style={{
-								fontWeight: "bold",
-								marginLeft: "12%",
-								fontSize: 30,
+								flexDirection: "row",
+								justifyContent: "space-around",
 							}}
 						>
-							feeling today?
-						</Text>
-					</View>
-					<View style={{ flex: 2, justifyContent: "center" }}>
-						<View style={{}}>
-							<View
-								style={{
-									flexDirection: "row",
-									justifyContent: "space-around",
-									marginBottom: "5%",
-								}}
-							>
-								<Text
-									style={{
-										borderColor: "black",
-										borderRadius: 1,
-										borderWidth: 1,
-									}}
-								>
-									Area
-								</Text>
-								<Text>Cuisine</Text>
-							</View>
-							<View
-								style={{
-									flexDirection: "row",
-									justifyContent: "space-around",
-								}}
-							>
-								<Text>Price</Text>
-								<Text>Rating</Text>
-							</View>
+							<Text>Price</Text>
+							<Text>Rating</Text>
 						</View>
 					</View>
 				</View>
-				<View style={{ flex: 2, backgroundColor: "powderblue" }}>
-					<Text>Location Card</Text>
+			</View>
+			<View
+				style={{
+					flex: 2,
+					backgroundColor: "powderBlue",
+				}}
+			>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: "#3C5A59",
+						borderTopLeftRadius: 50,
+						borderTopRightRadius: 50,
+					}}
+				>
+					<View
+						style={{
+							flex: 1,
+							marginHorizontal: "9%",
+							marginTop: "5%",
+						}}
+					>
+						<LocationCard location={location} />
+						<LocationCard location={location} />
+						<LocationCard location={location} />
+						<LocationCard location={location} />
+					</View>
 				</View>
 			</View>
 		</SafeAreaView>
